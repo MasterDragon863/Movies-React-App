391ed301e1b201d500b43f50ae7b300d76deba6d
Saad <varshanisaad@gmail.com>
2025-05-03 18:25:37 -0700

    Initialize Repo

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <main>
+      <div className='patteren' />
+
+      <div className='wrapper'>
+        <h1>Find  Movies Without a the Hassle</h1>
+      </div>
+    </main>
+  )
+}
+
+export default App
